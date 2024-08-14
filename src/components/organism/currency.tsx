87f436236252e0a9fb46b166e70ecfbe5f22c80c
jujusnake <southpoley@gmail.com>
2024-08-14 19:31:43 +0900

    Feat: Finished currency UI

---
+++ src/components/organism/currency.tsx
@@ -0,0 +1,101 @@
+import { Drawer, DrawerContent, DrawerHeader, DrawerTitle } from '@/components/ui/drawer';
+import { ComponentProps, LiHTMLAttributes, useEffect, useRef, useState } from 'react';
+import { Drawer as DrawerPrimitive } from 'vaul';
+import { Input } from '../ui/input';
+import { cn } from '@/lib/utils';
+
+const CurrencySelectDrawer = ({
+  onCurrencySelect,
+  ...props
+}: ComponentProps<typeof DrawerPrimitive.Root> & { onCurrencySelect?: (currency: string) => void }) => {
+  // Refs
+  const drawerContentRef = useRef<HTMLDivElement>(null);
+
+  // State
+  const [left, setLeft] = useState(0);
+  const [searchValue, setSearchValue] = useState('');
+
+  // Effects
+  useEffect(() => {
+    const ro = new ResizeObserver((entries) => {
+      const entry = entries[0];
+
+      // set left position so that it is centered on window
+      if (entry.contentRect.width > 500) {
+        setLeft((entry.contentRect.width - 500) / 2);
+      } else {
+        setLeft(0);
+      }
+    });
+
+    ro.observe(document.body);
+
+    return () => {
+      ro.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (props.open && window.visualViewport) {
+      window.visualViewport.addEventListener('resize', onVisualViewportChange);
+
+      return () => window.visualViewport?.removeEventListener('resize', onVisualViewportChange);
+    }
+  }, [props.open]);
+
+  function onVisualViewportChange() {
+    if (!drawerContentRef.current || !window.visualViewport) return;
+    const visualViewportHeight = window.visualViewport.height;
+
+    drawerContentRef.current.style.height = `${visualViewportHeight * 0.8}px`;
+  }
+
+  return (
+    <Drawer {...props}>
+      <DrawerContent
+        ref={drawerContentRef}
+        id="currency-select-drawer-content"
+        className="max-w-moduchongmu max-h-[80%] [&>div]:shrink-0"
+        style={{ left }}
+        aria-describedby={undefined}
+      >
+        <DrawerHeader>
+          <DrawerTitle className="mb-3">통화 선택</DrawerTitle>
+          <Input placeholder="통화 검색" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
+        </DrawerHeader>
+        <ul className="px-4 pt-2 pb-10 overflow-auto">
+          <CurrencyListItem onClick={() => onCurrencySelect?.('KRW(원)')}>KRW(원)</CurrencyListItem>
+          <CurrencyListItem>KRW(원)</CurrencyListItem>
+          <CurrencyListItem>KRW(원)</CurrencyListItem>
+          <CurrencyListItem>KRW(원)</CurrencyListItem>
+          <CurrencyListItem>KRW(원)</CurrencyListItem>
+          <CurrencyListItem>KRW(원)</CurrencyListItem>
+          <CurrencyListItem>KRW(원)</CurrencyListItem>
+          <CurrencyListItem>MIDDLE</CurrencyListItem>
+          <CurrencyListItem>KRW(원)</CurrencyListItem>
+          <CurrencyListItem>KRW(원)</CurrencyListItem>
+          <CurrencyListItem>KRW(원)</CurrencyListItem>
+          <CurrencyListItem>KRW(원)</CurrencyListItem>
+          <CurrencyListItem>KRW(원)</CurrencyListItem>
+          <CurrencyListItem>KRW(원)</CurrencyListItem>
+          <CurrencyListItem>KRW(원)</CurrencyListItem>
+          <CurrencyListItem>LAST</CurrencyListItem>
+        </ul>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+const CurrencyListItem = ({ className, ...props }: LiHTMLAttributes<HTMLLIElement>) => {
+  return (
+    <li
+      className={cn(
+        'border-[#ECECEC] w-full p-3 text-[15px]/[125%] font-medium even:bg-bg-base hover:bg-bg-base/50 active:bg-bg-front rounded-sm transition-colors',
+        className,
+      )}
+      {...props}
+    ></li>
+  );
+};
+
+export { CurrencySelectDrawer };
