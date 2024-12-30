fcb21e798610b9f44182513168942bb429d5ca0e
jujusnake <southpoley@gmail.com>
2024-12-30 16:33:13 +0900

    Feat: 날짜별 트랜잭션 뷰 완료

--- src/pages/trip/components/transaction-list.tsx
+++ src/pages/trip/components/transaction-list.tsx
@@ -4,6 +4,8 @@ import { useMemo } from 'react';
 import { TransactionCategoryType, TransactionItem } from '@/types/transaction';
 import EmptyIcon from '@/components/atoms/EmptyIcon';
 import InfinityScrollTrigger from '@/components/atoms/InfinityScrollTrigger';
+import { compareAsc, compareDesc, format } from 'date-fns';
+import { getDdayOrElapsedDay } from '@/lib/datetime';
 
 const CATEGORY_LABEL: Record<TransactionCategoryType, string> = {
   food: '식비',
@@ -64,44 +66,51 @@ const TransactionListByDate = ({ travelUid }: { travelUid: string }) => {
   // API Calls
   const { data } = useTransactionList(travelUid);
 
-  // Values
-  // const transactionsByDate = data?.pages?.reduce((acc: Record<string, any>, transaction: any) => {
-  //   const date = transaction.date;
-  //   if (!acc[date]) {
-  //     acc[date] = [];
-  //   }
-  //   acc[date].push(transaction);
-  //   return acc;
-  // }, {});
+  const transactions = useMemo(() => {
+    return data?.pages.reduce((acc: TransactionItem[], page) => {
+      const nextList = page?.transactionList ?? [];
+      return [...acc, ...nextList];
+    }, []);
+  }, [data]);
 
-  // console.log(transactionsByDate);
+  // Values
+  const transactionsByDate = useMemo(() => {
+    if (!transactions) return [];
+
+    const result: Record<string, TransactionItem[]> = {};
+    for (const item of transactions) {
+      const date = item.usedDate;
+      if (!result[date]) {
+        result[date] = [];
+      }
+      result[date].push(item);
+    }
+
+    return Object.entries(result).sort(([a], [b]) => compareDesc(a, b));
+  }, [transactions]);
 
   return (
     <>
-      <DailyExpenseBlock className="mb-5 last:mb-0">
-        <div className="flex items-center justify-between gap-2 mb-1">
-          <DailyExpenseTitle>05.02 (1일차)</DailyExpenseTitle>
-          <DailyExpenseAdd />
-        </div>
-        {/* <ExpenseItem title="점심식사" amount={12300} currency="₩" category="식비" mates={['남듀', '효고버섯']} />
-        <ExpenseItem title="점심식사" amount={12300} currency="₩" category="식비" mates={['남듀', '효고버섯']} /> */}
-      </DailyExpenseBlock>
-      <DailyExpenseBlock className="mb-5 last:mb-0">
-        <div className="flex items-center justify-between gap-2 mb-1">
-          <DailyExpenseTitle>05.02 (1일차)</DailyExpenseTitle>
-          <DailyExpenseAdd />
-        </div>
-        {/* <ExpenseItem title="점심식사" amount={12300} currency="₩" category="식비" mates={['남듀', '효고버섯']} />
-        <ExpenseItem title="점심식사" amount={12300} currency="₩" category="식비" mates={['남듀', '효고버섯']} /> */}
-      </DailyExpenseBlock>
-      <DailyExpenseBlock className="mb-5 last:mb-0">
-        <div className="flex items-center justify-between gap-2 mb-1">
-          <DailyExpenseTitle>05.02 (1일차)</DailyExpenseTitle>
-          <DailyExpenseAdd />
-        </div>
-        {/* <ExpenseItem title="점심식사" amount={12300} currency="₩" category="식비" mates={['남듀', '효고버섯']} />
-        <ExpenseItem title="점심식사" amount={12300} currency="₩" category="식비" mates={['남듀', '효고버섯']} /> */}
-      </DailyExpenseBlock>
+      {transactionsByDate.map(([date, transactions]) => (
+        <DailyExpenseBlock className="mb-5 last:mb-0" key={`travel-${travelUid}-daily-${date}`}>
+          <div className="flex items-center justify-between gap-2 mb-1">
+            <DailyExpenseTitle>{format(date, 'MM-dd')}</DailyExpenseTitle>
+            {/* <DailyExpenseAdd /> */}
+          </div>
+          {transactions.map((transaction) => (
+            <ExpenseItem
+              key={`travel-${travelUid}-daily-${date}-item-${transaction.uid}`}
+              title={transaction.content || CATEGORY_LABEL[transaction.category as TransactionCategoryType]}
+              amount={transaction.amount}
+              date={transaction.usedDate}
+              currency={transaction.currency}
+              category={CATEGORY_LABEL[transaction.category as TransactionCategoryType]}
+              mates={transaction.targetList}
+              payer={transaction.executorList[0]}
+            />
+          ))}
+        </DailyExpenseBlock>
+      ))}
     </>
   );
 };
